adcdb3e93ed960a0e7a6b3f9640dacc99ee0f78a
Vishal L <vishalkumar18ssb786@gmail.com>
2022-10-16 23:13:02 +0530

    -

---
+++ my-first.c
@@ -0,0 +1,10 @@
+// #include "stdio.h"
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int x;
+    scanf("%d",&x);
+    printf("this is my code and the variable value is \n");
+    return 0;
+}
